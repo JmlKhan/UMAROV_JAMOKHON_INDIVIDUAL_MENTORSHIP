2195739c3ceb657cc70ece32a46a932c21c5c2e4
Jamol <jmlkhon17@gmail.com>
2022-04-30 15:25:15 +0500

    adding model and fetching data

---
+++ DAL/Model/CityTemperatureModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class CityTemperatureModel
+    {
+        public List<weather> list { get; set; }
+       
+        public class weather
+        {
+            public Main main { get; set; }
+        }
+
+        public class Main
+        {
+            public double temp { get; set; }
+        
+        }
+
+        
+    }
+}
---
+++ DAL/WeatherApi.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class WeatherApi
+    {
+        public string GetWeather()
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f4b61da4bd8cbd9e4917a2c479fbb04f&q=Tashkent");
+                var json = web.DownloadString(url);
+                //var newJson = json[2];
+                var result = JsonConvert.DeserializeObject<CityTemperatureModel>(json);
+
+                var temp = result.list[0].main.temp.ToString();
+                return "ffsa";
+            }
+
+        }
+    }
+}
