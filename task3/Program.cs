4f3cc6a3901eb25c8d5a7ac5d80f5081361c3019
Jamol <jmlkhon17@gmail.com>
2022-04-30 15:23:54 +0500

    adding model and fetching api

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using DAL;
+
+//var newApi = new Api();
+
+//var result = newApi.GetResult();
+//result.Wait();
+var obj = new WeatherApi();
+var result = obj.GetWeather();
+Console.WriteLine(result);
